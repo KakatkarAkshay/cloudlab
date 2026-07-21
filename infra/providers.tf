dd7acc99d415ff9aa1bb4f71bbde638c2c871dc3
Akshay Kakatkar <me@kakatkarakshay.dev>
2026-07-21 09:15:00 +0530

    feat: establish Terraform configuration

---
+++ infra/providers.tf
@@ -0,0 +1,19 @@
+provider "oci" {
+  alias = "tenancy_1"
+
+  tenancy_ocid = var.tenancy_1_ocid
+  user_ocid    = var.tenancy_1_user_ocid
+  fingerprint  = var.oci_fingerprint
+  private_key  = var.oci_private_key
+  region       = var.oci_region
+}
+
+provider "oci" {
+  alias = "tenancy_2"
+
+  tenancy_ocid = var.tenancy_2_ocid
+  user_ocid    = var.tenancy_2_user_ocid
+  fingerprint  = var.oci_fingerprint
+  private_key  = var.oci_private_key
+  region       = var.oci_region
+}
