d751fb63136cc14981ca689335818e73fb7bff14
Akshay Kakatkar <me@kakatkarakshay.dev>
2026-07-21 23:01:21 +0530

    chore: remove Flux Helm migration

--- infra/providers.tf
+++ infra/providers.tf
@@ -40,12 +40,3 @@ provider "flux" {
     }
   }
 }
-
-provider "helm" {
-  kubernetes = {
-    host                   = talos_cluster_kubeconfig.cluster.kubernetes_client_configuration.host
-    cluster_ca_certificate = base64decode(talos_cluster_kubeconfig.cluster.kubernetes_client_configuration.ca_certificate)
-    client_certificate     = base64decode(talos_cluster_kubeconfig.cluster.kubernetes_client_configuration.client_certificate)
-    client_key             = base64decode(talos_cluster_kubeconfig.cluster.kubernetes_client_configuration.client_key)
-  }
-}
