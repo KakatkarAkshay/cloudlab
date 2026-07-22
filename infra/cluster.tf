142ca56a79f0f7d4e7b1eb78a8fc0ed9a8d010d5
Akshay Kakatkar <me@kakatkarakshay.dev>
2026-07-22 20:41:18 +0530

    feat: manage Talos image with Terraform

--- infra/cluster.tf
+++ infra/cluster.tf
@@ -1,7 +1,7 @@
 locals {
-  talos_image        = jsondecode(file("${path.module}/talos-image.json"))
   cluster_versions   = jsondecode(file("${path.module}/../cluster-versions.json"))
-  talos_image_object = "talos-${local.talos_image.version}-${local.talos_image.schematic_id}-oracle-arm64.oci"
+  talos_image_object = "talos-${local.cluster_versions.talos}-${talos_image_factory_schematic.cluster.id}-oracle-arm64.qcow2"
+  talos_image_path   = "${path.module}/build/${local.talos_image_object}"
   control_plane_ip   = cidrhost(var.tenancy_1_subnet_cidr, 10)
   worker_ip          = cidrhost(var.tenancy_2_subnet_cidr, 10)
   control_plane_ipv6 = cidrhost(cidrsubnet(module.tenancy_1_vcn.ipv6_cidr_block, 8, 0), 10)
@@ -13,6 +13,31 @@ locals {
   cluster_api_addresses = [
     for address in oci_network_load_balancer_network_load_balancer.control_plane.ip_addresses : address.ip_address
     if address.is_public
+  ]
+}
+
+resource "talos_image_factory_schematic" "cluster" {
+  schematic = file("${path.module}/talos-schematic.yaml")
+}
+
+data "talos_image_factory_urls" "oracle_arm64" {
+  talos_version = local.cluster_versions.talos
+  schematic_id  = talos_image_factory_schematic.cluster.id
+  platform      = "oracle"
+  architecture  = "arm64"
+}
+
+data "local_command" "talos_image" {
+  command = "curl"
+  arguments = [
+    "--fail",
+    "--location",
+    "--silent",
+    "--show-error",
+    "--create-dirs",
+    "--output",
+    local.talos_image_path,
+    data.talos_image_factory_urls.oracle_arm64.urls.disk_image,
   ]
 }
 
@@ -52,11 +77,12 @@ resource "oci_objectstorage_object" "talos_tenancy_1" {
   bucket    = oci_objectstorage_bucket.talos_tenancy_1.name
   namespace = data.oci_objectstorage_namespace.tenancy_1.namespace
   object    = local.talos_image_object
-  source    = "${path.module}/build/talos-oracle-arm64.oci"
+  source    = local.talos_image_path
+
+  depends_on = [data.local_command.talos_image]
 
   lifecycle {
     create_before_destroy = true
-    ignore_changes        = [source]
   }
 }
 
@@ -66,11 +92,12 @@ resource "oci_objectstorage_object" "talos_tenancy_2" {
   bucket    = oci_objectstorage_bucket.talos_tenancy_2.name
   namespace = data.oci_objectstorage_namespace.tenancy_2.namespace
   object    = local.talos_image_object
-  source    = "${path.module}/build/talos-oracle-arm64.oci"
+  source    = local.talos_image_path
+
+  depends_on = [data.local_command.talos_image]
 
   lifecycle {
     create_before_destroy = true
-    ignore_changes        = [source]
   }
 }
 
@@ -78,7 +105,7 @@ resource "oci_core_image" "talos_tenancy_1" {
   provider = oci.tenancy_1
 
   compartment_id = var.tenancy_1_compartment_ocid
-  display_name   = "Talos ${local.talos_image.version} ARM64"
+  display_name   = "Talos ${local.cluster_versions.talos} ARM64"
   launch_mode    = "PARAVIRTUALIZED"
 
   image_source_details {
@@ -87,7 +114,8 @@ resource "oci_core_image" "talos_tenancy_1" {
     bucket_name              = oci_objectstorage_bucket.talos_tenancy_1.name
     object_name              = oci_objectstorage_object.talos_tenancy_1.object
     operating_system         = "Talos"
-    operating_system_version = trimprefix(local.talos_image.version, "v")
+    operating_system_version = trimprefix(local.cluster_versions.talos, "v")
+    source_image_type        = "QCOW2"
   }
 
   lifecycle {
@@ -99,7 +127,7 @@ resource "oci_core_image" "talos_tenancy_2" {
   provider = oci.tenancy_2
 
   compartment_id = var.tenancy_2_compartment_ocid
-  display_name   = "Talos ${local.talos_image.version} ARM64"
+  display_name   = "Talos ${local.cluster_versions.talos} ARM64"
   launch_mode    = "PARAVIRTUALIZED"
 
   image_source_details {
@@ -108,7 +136,8 @@ resource "oci_core_image" "talos_tenancy_2" {
     bucket_name              = oci_objectstorage_bucket.talos_tenancy_2.name
     object_name              = oci_objectstorage_object.talos_tenancy_2.object
     operating_system         = "Talos"
-    operating_system_version = trimprefix(local.talos_image.version, "v")
+    operating_system_version = trimprefix(local.cluster_versions.talos, "v")
+    source_image_type        = "QCOW2"
   }
 
   lifecycle {
@@ -378,7 +407,7 @@ resource "oci_network_load_balancer_listener" "talos" {
 }
 
 resource "talos_machine_secrets" "cluster" {
-  talos_version = local.talos_image.version
+  talos_version = local.cluster_versions.talos
 }
 
 data "talos_machine_configuration" "control_plane" {
@@ -386,13 +415,18 @@ data "talos_machine_configuration" "control_plane" {
   machine_type       = "controlplane"
   cluster_endpoint   = "https://${local.cluster_api_ip}:6443"
   machine_secrets    = talos_machine_secrets.cluster.machine_secrets
-  talos_version      = local.talos_image.version
+  talos_version      = local.cluster_versions.talos
   kubernetes_version = local.cluster_versions.kubernetes
 
   config_patches = [
     yamlencode({
       machine = {
         certSANs = local.cluster_api_addresses
+        kernel = {
+          modules = [
+            { name = "binfmt_misc" },
+          ]
+        }
         sysctls = {
           "net.ipv4.ip_forward"          = "1"
           "net.ipv6.conf.all.forwarding" = "1"
@@ -446,13 +480,18 @@ data "talos_machine_configuration" "worker" {
   machine_type       = "worker"
   cluster_endpoint   = "https://${local.cluster_api_ip}:6443"
   machine_secrets    = talos_machine_secrets.cluster.machine_secrets
-  talos_version      = local.talos_image.version
+  talos_version      = local.cluster_versions.talos
   kubernetes_version = local.cluster_versions.kubernetes
 
   config_patches = [
     yamlencode({
       machine = {
         certSANs = local.cluster_api_addresses
+        kernel = {
+          modules = [
+            { name = "binfmt_misc" },
+          ]
+        }
         sysctls = {
           "net.ipv4.ip_forward"          = "1"
           "net.ipv6.conf.all.forwarding" = "1"
