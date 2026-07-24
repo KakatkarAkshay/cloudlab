5b204318fa66ac9bcefed9efd828646b7ca8a8e3
Akshay Kakatkar <me@kakatkarakshay.dev>
2026-07-24 12:10:44 +0530

    chore: tune cluster resource requests

--- infra/flux.tf
+++ infra/flux.tf
@@ -1,8 +1,9 @@
 resource "flux_bootstrap_git" "cluster" {
   depends_on = [talos_cluster_kubeconfig.cluster]
 
-  embedded_manifests = true
-  path               = "clusters/cloudlab"
+  embedded_manifests     = true
+  kustomization_override = file("${path.module}/flux-kustomization.yaml")
+  path                   = "clusters/cloudlab"
   components_extra = [
     "image-automation-controller",
     "image-reflector-controller",
