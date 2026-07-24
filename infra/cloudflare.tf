cf6d62053b95404101b5e154d1190239ee459e4c
Akshay Kakatkar <me@kakatkarakshay.dev>
2026-07-24 11:56:45 +0530

    feat: manage gateway DNS with external-dns

--- infra/cloudflare.tf
+++ infra/cloudflare.tf
@@ -10,35 +10,6 @@ locals {
   cloudflare_account_id = data.cloudflare_accounts.current.result[0].id
 }
 
-resource "cloudflare_zero_trust_tunnel_cloudflared" "cloudlab" {
-  account_id = local.cloudflare_account_id
-  name       = "CloudLab"
-  config_src = "cloudflare"
-}
-
-resource "cloudflare_zero_trust_tunnel_cloudflared_config" "cloudlab" {
-  account_id = local.cloudflare_account_id
-  tunnel_id  = cloudflare_zero_trust_tunnel_cloudflared.cloudlab.id
-  source     = "cloudflare"
-
-  config = {
-    ingress = [
-      {
-        hostname = "*.${var.cloudflare_zone}"
-        service  = "http://traefik.traefik.svc.cluster.local:80"
-      },
-      {
-        service = "http_status:404"
-      },
-    ]
-  }
-}
-
-data "cloudflare_zero_trust_tunnel_cloudflared_token" "cloudlab" {
-  account_id = local.cloudflare_account_id
-  tunnel_id  = cloudflare_zero_trust_tunnel_cloudflared.cloudlab.id
-}
-
 data "cloudflare_account_api_token_permission_groups_list" "zone_read" {
   account_id = local.cloudflare_account_id
   name       = "Zone Read"
